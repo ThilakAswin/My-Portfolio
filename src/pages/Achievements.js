d5fc2ca679a0c2cf8e0fd2b6252d1591fd123806
Thilak <thilakaswin7@gmail.com>
2024-03-03 22:21:49 +0530

    super

--- src/pages/Achievements.js
+++ src/pages/Achievements.js
@@ -2,9 +2,9 @@
 
 function Achievements(){
 
- const certifications = ["Successfully completed the course SQL for Beginners: Learn SQL using MySQL and Database Design on Udemy.",
-"Successfully completed the course Introduction to Data Structures & Algorithms in Java on Udemy.",
-"Successfully completed the course Modern Java-Learn Java 8 features by coding it on Udemy.",
+ const certifications = ["Successfully completed the course SQL for Beginners: Learn SQL using MySQL and Database Design on Udemy. \n ",
+"Successfully completed the course Introduction to Data Structures & Algorithms in Java on Udemy. \n ",
+"Successfully completed the course Modern Java-Learn Java 8 features by coding it on Udemy. \n ",
 "Successfully completed the course Agile Fundamentals: Including Scrum & Kanban on Udemy."]
 
 const prizes = ["Won Second Prize on MindSpark Hackathon Organized By IUCEE",
@@ -13,7 +13,9 @@ const prizes = ["Won Second Prize on MindSpark Hackathon Organized By IUCEE",
     <div className="achievementsArea">
         <h3>Certifications</h3>
         <ul>
-            {certifications.map(detail => <li>{detail}</li>)}
+            {certifications.map(detail => 
+                <div className="newLine"><li>{detail}</li></div>
+        )}
         </ul>
         <h3>Prizes</h3>
         <ul>
